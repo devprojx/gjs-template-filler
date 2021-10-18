d5e2cfe714bcbd872628b65ff2d9b5e27533ba4e
Yanik Black <yanikblake@gmail.com>
2021-10-18 01:56:14 -0500

    feat(index.js): implemented formatter in populateHTMLTemplate function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import cheerio from "cheerio";
+import formatData from "./formatter";
 
 /**
  * Populate html template and return populated html string
@@ -48,6 +49,19 @@ const _populateTemplate = ($el, data) => {
 	//Remove element or set defualt value if data is not found
 	if (!data) return !defaultValue ? $el.remove() : $el.html(defaultValue);
 
+	const _getFormatOptions = ($el) => {
+		const opts = {
+			type: $el.attr("data-type"),
+			symbol: $el.attr("data-symbol"),
+			precision: $el.attr("data-precision"),
+			seperator: $el.attr("data-number-seperator"),
+			format: $el.attr("data-format"),
+			append: $el.attr("data-append"),
+			prepend: $el.attr("data-prepend"),
+		};
+		return opts;
+	};
+
 	if (Array.isArray(data)) {
 		//Remove table or container el if no data is provided
 		if (data.length == 0) {
@@ -56,11 +70,16 @@ const _populateTemplate = ($el, data) => {
 		}
 
 		if (typeof data[0] !== "object") {
-			const seperator = $el.attr("data-seperator") || " ";
-			$el.find(`[data-value]`).html(data.join(seperator));
-
 			const isList = $el.is("ul") || $el.is("ol");
-			if (isList) $el.find(`[data-value]`).html(data.map((v) => `<li>${v || "N/A"}</li>`));
+			if (isList) {
+				const $li = $el.find(`[data-value]`);
+				$li.html(data.map((v) => `<li>${formatData(v, _getFormatOptions($li)) || "N/A"}</li>`));
+				return;
+			}
+
+			const seperator = $el.attr("data-seperator") || " ";
+			const $dataEl = $el.find(`[data-value]`);
+			$dataEl.html(data.map((v) => formatData(v, _getFormatOptions($dataEl))).join(seperator));
 			return;
 		}
 
@@ -69,28 +88,28 @@ const _populateTemplate = ($el, data) => {
 
 		const props = [];
 		//Get the expected data points from table headers
-		$el.find(`th[data-key]`).each((i, tableHeaderEl) => {
-			tableHeaderEl = $el.find(tableHeaderEl);
-			const key = tableHeaderEl.attr("data-key");
-			const defaultValue = tableHeaderEl.attr("data-default") || "";
-			props.push({ key, defaultValue });
+		$el.find(`th[data-key]`).each((i, $tableHeaderEl) => {
+			$tableHeaderEl = $el.find($tableHeaderEl);
+			const key = $tableHeaderEl.attr("data-key");
+			const defaultValue = $tableHeaderEl.attr("data-default") || "";
+			props.push({ key, defaultValue, options: _getFormatOptions($tableHeaderEl) });
 		});
 
 		//Construct <td></td> tag based on the property associated with the header
-		const buildCell = (data, k, defaultValue) => {
+		const buildCell = (data, k, defaultValue, options) => {
 			const keys = k.split(".") || [];
 			if (keys.length > 1) {
 				const key = keys.shift();
 				return buildCell(data[key], keys.join("."), defaultValue);
 			}
 
-			return `<td> ${!data || !data[k] ? `${defaultValue}` : `${data[k]}`} </td>`;
+			return `<td> ${!data || !data[k] ? `${defaultValue}` : `${formatData(data[k], options)}`} </td>`;
 		};
 
 		//Construct table rows
 		const tableData = [];
 		data.forEach((el) => {
-			const tds = props.map(({ key, defaultValue }) => buildCell(el, key, defaultValue));
+			const tds = props.map(({ key, defaultValue, options }) => buildCell(el, key, defaultValue, options));
 			tableData.push(`<tr>${tds.join("")}</tr>`);
 		});
 
@@ -99,7 +118,7 @@ const _populateTemplate = ($el, data) => {
 		return;
 	}
 
-	$el.html(data);
+	$el.html(formatData(data, _getFormatOptions($el)));
 };
 
 export { populateHTMLTemplate };
